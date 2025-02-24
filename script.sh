09faae7e066403d366894b111601a337468fa679
Jatin Mehrotra <49259715+jatinmehrotra@users.noreply.github.com>
2025-02-24 10:43:11 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,3 @@
-echo "jatin mehrotra test for codepipeline ec2 deployment action successful" > /var/www/html/index.html
+# echo "jatin mehrotra test for codepipeline ec2 deployment action successful" > /var/www/html/index.html
+
+echo "codepipeline ec2 deployment" > /var/www/html/index.html
